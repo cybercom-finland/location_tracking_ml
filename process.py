e28e533cdcdffa5827c615c665dc043030399f11
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-03-25 01:41:10 +0200

    Added some initial LSTM stuff with roughly correct configuration with displaced expected output and L2 loss. Does not work yet, because lists are not arrays.

--- process.py
+++ process.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 import tensorflow as tf
+from tensorflow.models.rnn import rnn, rnn_cell
+import numpy as np
+
 import json
 import itertools
 
@@ -52,9 +55,117 @@ with open('tracks.m', 'w') as octaveFile:
 # recorded game (note the interval between samples).
 # This is just a simple proof of concept, so we don't take too much headache of this.
 
-
 input = positionTracks.items()
 train = list(itertools.islice(input, 0, None, 3))
 test = list(itertools.islice(input, 1, None, 3))
 validation = list(itertools.islice(input, 2, None, 3))
-print str(train)
+
+# Each of the sets have 22677 positions (x,y) for 23 players.
+# We'll divide these into minibatches of size 20, getting 1133 full minibatches.
+
+# Let's try this first with fully connected LSTM layers. Note that the implementation is not yet
+# as specified in the README. This is work in progress.
+
+batch_size = 20;
+
+# The input is position and velocity for each player. Velocity is 0 if not otherwise possible to calculate.
+# TODO: Flag input for valid or invalid position.
+
+# Parameters
+learning_rate = 0.001
+training_iters = 100000
+display_step = 10
+
+# Network Parameters
+n_input = 23*2
+n_steps = 28 # timesteps
+n_hidden = 128 # hidden layer num of features
+n_output = n_input
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_steps, n_input])
+# Tensorflow LSTM cell requires 2x n_hidden length (state & cell)
+istate = tf.placeholder("float", [None, 2*n_hidden])
+y = tf.placeholder("float", [None, n_output])
+
+# Define weights
+weights = {
+    'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])), # Hidden layer weights
+    'out': tf.Variable(tf.random_normal([n_hidden, n_output]))
+}
+biases = {
+    'hidden': tf.Variable(tf.random_normal([n_hidden])),
+    'out': tf.Variable(tf.random_normal([n_output]))
+}
+
+def getNextTrainingBatch(step):
+    disp = step * batch_size % (len(train) - (batch_size - 2))
+    return train[disp:disp+batch_size], train[disp+1:disp+batch_size+1]
+
+def RNN(_X, _istate, _weights, _biases):
+
+    # input shape: (batch_size, n_steps, n_input)
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    # Reshape to prepare input to hidden activation
+    _X = tf.reshape(_X, [-1, n_input]) # (n_steps*batch_size, n_input)
+    # Linear activation
+    _X = tf.matmul(_X, _weights['hidden']) + _biases['hidden']
+
+    # Define a lstm cell with tensorflow
+    lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(0, n_steps, _X) # n_steps * (batch_size, n_hidden)
+
+    # Get lstm cell output
+    outputs, states = rnn.rnn(lstm_cell, _X, initial_state=_istate)
+
+    # Linear activation
+    # Get inner loop last output
+    return tf.matmul(outputs[-1], _weights['out']) + _biases['out']
+
+pred = RNN(x, istate, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.l2_loss(pred-y)) # L2 loss for regression
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Adam Optimizer
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    
+    # FIXME: This is still work in progress....
+    while False: # step * batch_size < training_iters:
+        batch_xs, batch_ys = getNextTrainingBatch(step - 1)
+        # Reshape data to get 28 seq of 28 elements
+        batch_xs = batch_xs.reshape((batch_size, n_steps, n_input))
+        # Fit training using batch data
+        sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys,
+                                       istate: np.zeros((batch_size, 2*n_hidden))})
+        if step % display_step == 0:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys,
+                                                istate: np.zeros((batch_size, 2*n_hidden))})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x: batch_xs, y: batch_ys,
+                                             istate: np.zeros((batch_size, 2*n_hidden))})
+            print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
+                  ", Training Accuracy= " + "{:.5f}".format(acc)
+        step += 1
+    print "Optimization Finished!"
+    # Calculate accuracy for the test data
+    test_len = len(test) - 1
+    
+    # FIXME: This is still work in progress....
+    #test_data = test[:test_len].reshape((-1, n_steps, n_input))
+    #test_label = test[1:test_len + 1]
+    #print "Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
+    #                                                         istate: np.zeros((test_len, 2*n_hidden))})
